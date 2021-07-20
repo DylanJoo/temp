ed381a216776c47dd002ef0a834ea84fd0537c89
DylanJoo <dylanjootw@gmail.com>
2021-07-20 18:44:31 +0800

    fix

--- treccast/convert_eval_json_to_tsv.py
+++ treccast/convert_eval_json_to_tsv.py
@@ -2,12 +2,27 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser()
-a = open("/content/y3_automatic_results_1000.v1.0.run", 'r')
-b = open("/content/y3_automatic_results_1000.v1.0.run.corrected", 'w')
-c = open("/content/2021_automatic_evaluation_topics_v1.0.tsv", 'r')
-d = open("/content/2021_automatic_evaluation_topics_v1.0.tsv.corrected", 'w')
+parser.add_argument("--officail_dev", type=str, required=True)
+parser.add_argument("--officail_baseline_run", type=str, required=True)
+
+# y3_automatic_results_1000.v1.0.run
+# 2021_automatic_evaluation_topics_v1.0.tsv.corrected
 
 flag = -1
+def dev_check(path):
+    dev_json = json.load(open(path, 'r'))
+    dev_tsv_correct
+    for topic in dev_json:
+        topicid = topic['number']
+        for i, turn in enumerate(topic['turn']):
+            docid = str(turn['canonical_result_id']) + "-" + str(turn['passage_id'])
+            turnid = str(turn['number'])
+            if i+1 != turn['number']:
+                print("[CORRECTION]: {}_{} --> {}_{}".format(
+                    topicid, turnid, topicid, i+1))
+                turnid = i+1
+            qid = str(topicid) + "_" + str(turnid)
+            u.write("{}\t{}\n".format(qid, docid))
 
 for line in a:
     qid, _, _, rank, _, _ = line.strip().split(' ')
@@ -54,12 +69,6 @@ u = open("/content/2021_automatic_evaluation_topics_v1.0.tsv", 'w')
 data = open("/content/2021_automatic_evaluation_topics_v1.0.json", 'r')
 json_data = json.load(data)
 
-for topic in json_data:
-    topicid = topic['number']
-    for turn in topic['turn']:
-        qid = str(topicid) + "_" + str(turn['number'])
-        docid = str(turn['canonical_result_id']) + "-" + str(turn['passage_id'])
-        u.write("{}\t{}\n".format(qid, docid))
 
 u.close()
 data.close()
