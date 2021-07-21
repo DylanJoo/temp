1352d8d95533f5106b79d7a610fff12ca7002279
DylanJoo <dylanjootw@gmail.com>
2021-07-21 14:59:53 +0800

    fix

--- treccast/correct_eval_and_baseline.py
+++ treccast/correct_eval_and_baseline.py
@@ -5,29 +5,37 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument("--official_dev_json", type=str)
 parser.add_argument("--official_dev_run", type=str)
+parser.add_argument("-pnr", "--passage_number_removal", action="store_true", default=True)
 parser.add_argument("--trec", action="store_true", default=True)
 args = parser.parse_args()
 
 # y3_automatic_results_1000.v1.0.run
 # 2021_automatic_evaluation_topics_v1.0.tsv.correcte
 
-def dev_check(path, trec):
+def dev_check(path, pnr):
     dev_json = json.load(open(path, 'r'))
-    dev_tsv_corrected = open(path + "_corrected.trec", 'w')
+    dev_corrected = open(path + "_corrected.trec", 'w')
+    if pnr:
+        dev_corrected_doc = open(path + "_doc_corrected.trec", 'w')
     for topic in dev_json:
         topicid = topic['number']
         for i, turn in enumerate(topic['turn']):
-            docid = str(turn['canonical_result_id']) + "-" + str(turn['passage_id'])
+            docid = str(turn['canonical_result_id'])
+            doc_passage_id = str(turn['canonical_result_id']) + "-" + str(turn['passage_id'])
             turnid = str(turn['number'])
             if i+1 != turn['number']:
                 print("[CORRECTION]: {}_{} --> {}_{}".format(
                     topicid, turnid, topicid, i+1))
                 turnid = i+1
             qid = str(topicid) + "_" + str(turnid)
-            if trec:
-                dev_tsv_corrected.write("{} {} {} {}\n".format(qid, 0, docid, 1))
-            else:
-                dev_tsv_corrected.write("{}\t{}\n".format(qid, docid))
+
+            dev_corrected.write("{} {} {} {}\n".format(qid, 0, doc_passage_id, 1))
+            dev_corrected.write("{}\t{}\n".format(qid, docid))
+            # doc only
+            if pnr:
+                dev_corrected_doc.write("{} {} {} {}\n".format(qid, 0, docid, 1))
+                dev_corrected_doc.write("{}\t{}\n".format(qid, docid))
+
 def baseline_run_check(path):
     run_baseline = open(path, 'r')
     run_baseline_corrected = open(path + "_corrected", 'w')
@@ -48,6 +56,6 @@ def baseline_run_check(path):
             line = "{} Q0 {} {} {} {}\n".format(qid, doc_passage_id, rank, score, name)
             run_baseline_corrected.write(line)
 
-dev_check(args.official_dev_json, args.trec)
+dev_check(args.official_dev_json, args.pnr)
 baseline_run_check(args.official_dev_run)
 print("DONE")
