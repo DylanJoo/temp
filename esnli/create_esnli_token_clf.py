9bc4c59e44eb11e0f823b0fb030d466ff01cf6ab
DylanJoo <dylanjootw@gmail.com>
2021-11-30 18:11:10 +0800

    update parsing codes

--- esnli/create_esnli_token_clf.py
+++ esnli/create_esnli_token_clf.py
@@ -6,10 +6,12 @@ import argparse
 import collections
 from spacy.lang.en import English
 
+from transformers import AutoTokenizer
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-out", "--path_output", type=str)
-# parser.add_argument("-sentA", "--path_sentenceA", type=str)
-# parser.add_argument("-sentB", "--path_sentenceB", type=str)
+parser.add_argument("-sentA", "--path_sentenceA", type=str)
+parser.add_argument("-sentB", "--path_sentenceB", type=str)
 parser.add_argument("-highlightA", "--path_highlightA", type=str)
 parser.add_argument("-highlightB", "--path_highlightB", type=str)
 parser.add_argument("-label", "--path_labels", type=str)
@@ -18,40 +20,65 @@ args = parser.parse_args()
 
 nlp = English()
 
-def keyword_extraction(tgtA, tgtB, highlightA=False):
+def keyword_extraction(srcA, srcB, tgtA, tgtB, highlightA=False):
     """ Convert the "starred marks" to the separated list of elements. 
     Noted that, the default highlightA is False, only extract highlight B for simplicity.
 
     [TODO] position-sensitive keyword
     [TODO] first tokenize with word-level, and extract the marked tokens.
     """
-    p_highlight = re.compile(r"[\*].*?[\*]")
-    p_punct = re.compile("[" + re.escape(string.punctuation) + "]")
-    
+    tgtA = srcA if tgtA is None else tgtA
+    tgtB = srcB if tgtB is None else tgtB
 
-    tokens_A_hl, tokens_B_hl = [], []
-    # convert the star labels to tokens (sentenceA/highlightA)
-    findings = p_highlight.findall(tgtA)
-    for token in findings:
-        token = p_punct.sub("", token)
-        tokens_A_hl += token.split()
+    # p_highlight = re.compile(r"[\*].*?[\*]")
+    # p_punct = re.compile("[" + re.escape(string.punctuation) + "]")
+    #
+    # tokens_A_hl, tokens_B_hl = [], []
+    # # convert the star labels to tokens (sentenceA/highlightA)
+    # findings = p_highlight.findall(tgtA)
+    # for token in findings:
+    #     token = p_punct.sub("", token)
+    #     tokens_A_hl += token.split()
+    #
+    # # convert the star labels to tokens (sentenceB/highlightB)
+    # findings = p_highlight.findall(tgtB)
+    # for token in findings:
+    #     token = p_punct.sub("", token)
+    #     tokens_B_hl += token.split()
 
-    # convert the star labels to tokens (sentenceB/highlightB)
-    findings = p_highlight.findall(tgtB)
-    for token in findings:
-        token = p_punct.sub("", token)
-        tokens_B_hl += token.split()
+    # tgtA = tgtA.replace("*", "")
+    # tgtB = tgtB.replace("*", "")
+    # tokens_A = [tok.text for tok in nlp(tgtA)]
+    # tokens_B = [tok.text for tok in nlp(tgtB)]
 
-    tgtA = tgtA.replace("*", "")
-    tgtB = tgtB.replace("*", "")
-    tokens_A = [tok.text for tok in nlp(tgtA)]
-    tokens_B = [tok.text for tok in nlp(tgtB)]
+    def highlight_process(sentence):
+        tokens = list()
+        tokens_hl = list()
+        labels = list()
+        hl = 0
+
+        for tok in nlp(sentence):
+            if tok.text == "*":
+                hl = 0 if hl else 1
+            else:
+                tokens_hl += [tok.text] if hl else []
+                labels += [1] if hl else [0]
+                tokens += [tok.text]
+
+        return tokens, tokens_hl, labels
+
+    tokens_A, tokens_A_hl, labels_A = highlight_process(tgtA)
+    tokens_B, tokens_B_hl, labels_B = highlight_process(tgtB)
+
+    if highlightA is False:
+        labels_A = [0] * len(labels_A)
+        tokens_A_hl = []
 
     return {'sentA': ' '.join(tokens_A), 
             'sentB': ' '.join(tokens_B), 
-            'keywordsA': ' '.join(tokens_A_hl), 
-            'keywordsB': ' '.join(tokens_B_hl),
-            'labels': []}
+            'keywordsA': tokens_A_hl, 
+            'keywordsB': tokens_B_hl,
+            'labels': labels_A + [-1] + labels_B}
 
 def create_highlight_list(args):
 
@@ -61,11 +88,17 @@ def create_highlight_list(args):
         return data
 
     data = collections.OrderedDict()
+    data['sentenceA'] = readlines(args.path_sentenceA)
+    data['sentenceB'] = readlines(args.path_sentenceB)
     data['highlightA'] = readlines(args.path_highlightA)
     data['highlightB'] = readlines(args.path_highlightB)
     data['label'] = readlines(args.path_labels)
 
     if args.class_selected != 'all':
+        data['sentenceA'] = [h for (h, l) in zip(data['sentenceA'], data['label']) \
+                if l in args.class_selected]
+        data['sentenceB'] = [h for (h, l) in zip(data['sentenceB'], data['label']) \
+                if l in args.class_selected]
         data['highlightA'] = [h for (h, l) in zip(data['highlightA'], data['label']) \
                 if l in args.class_selected]
         data['highlighB'] = [h for (h, l) in zip(data['highlightB'], data['label']) \
@@ -73,8 +106,9 @@ def create_highlight_list(args):
         data['label'] = [l for  l in data['label'] if l in args.class_selected]
 
     with open(args.path_output, 'w') as f:
-        for idx, (hla, hlb) in enumerate(zip(data['highlightA'], data['highlightB'])):
-            example = keyword_extraction(tgtA=hla, tgtB=hlb)
+        for sa, sb, hla, hlb in zip(data['sentenceA'], data['sentenceB'], \
+                                    data['highlightA'], data['highlightB']):
+            example = keyword_extraction(srcA=sa, srcB=sb, tgtA=hla, tgtB=hlb)
             f.write(json.dumps(example) + '\n')
 
 create_highlight_list(args)
--- esnli/run_create_highlight_list.sh
+++ esnli/run_create_highlight_list.sh
@@ -1,12 +1,16 @@
 split=$1
 python3 create_esnli_token_clf.py \
+    -sentA preprocessed/${split}/sentenceA.txt \
+    -sentB preprocessed/${split}/sentenceB.txt \
     -highlightA preprocessed/${split}/highlightA.txt \
     -highlightB preprocessed/${split}/highlightB.txt \
     -label preprocessed/${split}/label.txt \
-    -out preprocessed/${split}/esnli_sents_highlight_contradict.jsonl \
+    -out preprocessed/${split}/esnli_sents_highlight_all.jsonl \
     -class 'all'
 
 python3 create_esnli_token_clf.py \
+    -sentA preprocessed/${split}/sentenceA.txt \
+    -sentB preprocessed/${split}/sentenceB.txt \
     -highlightA preprocessed/${split}/highlightA.txt \
     -highlightB preprocessed/${split}/highlightB.txt \
     -label preprocessed/${split}/label.txt \
