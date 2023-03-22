20df8bb71d634d62ff26cc54cf88da5b7daca255
DylanJoo <dylanjootw@gmail.com>
2023-03-22 12:28:25 +0800

    move document-segmentation

--- document-segmentation/README.md
+++
@@ -1,4 +0,0 @@
-JH: 
-I will using cross-segment bert at the folder
-The upstream of my remote branch is yours, so you should merge my branch after a complete my model.
-
--- document-segmentation/download_wiki.sh
+++
@@ -1,3 +0,0 @@
-# Download wiki 727 data
-wget https://www.dropbox.com/sh/k3jh0fjbyr0gw0a/AACKW_gsxUf282QqrfH3yD10a/wiki_727K.tar.bz2
-nohup tar -xvf wiki_727K.tar.bz2 >> wiki.log & 
